c13c85890d284eb559dc707456b79a01e750dfb5
DmitriyMinets <118934924+DmitriyMinets@users.noreply.github.com>
2023-04-01 12:07:46 +0300

    Выполнено ДЗ

--- Rocky/Controllers/ApplicationTypeController.cs
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -21,7 +21,7 @@ namespace Rocky.Controllers
 
         //GET-CREATE
         public IActionResult Create()
-        {    
+        {
             return View();
         }
 
@@ -30,13 +30,76 @@ namespace Rocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            if(ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 _db.ApplicationType.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        //GET-EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST-EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET-DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST-EDIT
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Rocky/Models/ApplicationType.cs
+++ Rocky/Models/ApplicationType.cs
@@ -6,6 +6,6 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
     }
 }
