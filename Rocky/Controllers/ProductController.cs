e0a6f87fa441d7d77e5f0372481bcfbeef0f65af
DmitriyMinets <118934924+DmitriyMinets@users.noreply.github.com>
2023-04-01 16:15:31 +0300

    Коммит перед установкой

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+            foreach(var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(x => x.Id == obj.CategoryId);
+            }
+            return View(objList);
+        }
+
+        //GET-UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(x => new SelectListItem
+            //{
+            //    Text = x.Name,
+            //    Value = x.Id.ToString()
+            //});
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+            //Product product = new();
+
+            ProductVM ProductVM = new ProductVM
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+
+            if(id == null) 
+            {
+                //this is for create
+                return View(ProductVM);
+            }
+            else
+            {
+                ProductVM.Product = _db.Product.Find(id);
+                if(ProductVM.Product == null) 
+                {
+                    return NotFound();
+                }
+                return View(ProductVM);
+            }
+        }
+
+        //POST-UPSERT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //GET-DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST-EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
